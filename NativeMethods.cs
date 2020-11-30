f41914300e141e1dc980fcc54c283ad16f2e7f30
Evulpes <54101182+Evulpes@users.noreply.github.com>
2020-11-30 00:53:39 +0000

    Add project files.

---
+++ NativeMethods.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace CRC32
+{
+    public class NativeMethods
+    {
+        protected static class Libloaderapi
+        {
+            [DllImport("kernel32", CharSet = CharSet.Ansi)]
+            public static extern IntPtr GetProcAddress(IntPtr hModule, string lpProcName);
+            [DllImport("kernel32", SetLastError = true)]
+            public static extern IntPtr GetModuleHandleA(string lpModuleName);
+        }
+        protected static class Debugapi
+        {
+            [DllImport("kernel32", SetLastError = true)]
+            public static extern bool IsDebuggerPresent();
+        }
+        protected static class Processthreadsapi
+        {
+            [DllImport("kernel32", SetLastError = true)]
+            public static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, out IntPtr lpThreadId);
+            [DllImport("Kernel32", SetLastError = true)]
+            public static extern bool GetExitCodeThread(IntPtr hThread, out uint lpExitCode);
+        }
+        protected static class Memoryapi
+        {
+            [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+            public static extern IntPtr VirtualAlloc(IntPtr lpAddress, uint dwSize, Winnt.AllocationType flAllocationType, Winnt.MemoryProtection flProtect);
+
+            [DllImport("kernel32.dll", SetLastError = true)]
+            public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, int nSize, out IntPtr lpNumberOfBytesWritten);
+
+        }
+        protected static class Winnt
+        {
+            public enum AllocationType
+            {
+                MEM_COMMIT = 0x1000,
+                MEM_FREE = 0x10000,
+                MEM_RESERVE = 0x2000,
+            }
+            public enum ProcessAccessFlags
+            {
+                PROCESS_ALL_ACCESS = 0xFFFF,
+            }
+            public enum MemoryProtection : uint
+            {
+                PAGE_EXECUTE = 0x10,
+                PAGE_EXECUTE_READ = 0x20,
+                PAGE_EXECUTE_READWRITE = 0x40,
+                PAGE_EXECUTE_WRITECOPY = 0x80,
+                PAGE_NOACCESS = 0x01,
+                PAGE_READONLY = 0x02,
+                PAGE_READWRITE = 0x04,
+                PAGE_WRITECOPY = 0x08,
+                PAGE_TARGETS_INVALID = 0x40000000,
+                PAGE_TARGETS_NO_UPDATE = 0x40000000,
+                PAGE_GUARD = 0x100,
+                PAGE_NOCACHE = 0x200,
+                PAGE_WRITECOMBINE = 0x400,
+
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace CRC32
+{
+    class Program : NativeMethods
+    {
+
+        static IntPtr allocLoc;
+        static readonly byte[] crcCheckCode = new byte[]
+        {
+            0x55,                                           //push rbp
+            0x48, 0x89, 0xe5,                               //mov rbp, rsp,
+            0x48, 0x8b, 0x49, 0x09,                         //mov rcx, [rcx+9]
+            0xf2, 0x48, 0x0f, 0x38, 0xf1, 0xc1,             //crc32 rax, rcx
+            0x48, 0x89, 0xec,                               //mov rsp, rbp
+            0x5d,                                           //pop rbp
+            0xc3                                            //ret
+        };
+        static void Main()
+        {
+            allocLoc = Memoryapi.VirtualAlloc(IntPtr.Zero, (uint)crcCheckCode.Length, Winnt.AllocationType.MEM_COMMIT, Winnt.MemoryProtection.PAGE_EXECUTE_READWRITE);
+            if (allocLoc == IntPtr.Zero)
+                throw new Exception($"Failed on VirtualAlloc with System Error Code {Marshal.GetLastWin32Error()}");
+
+            if(!Memoryapi.WriteProcessMemory(Process.GetCurrentProcess().Handle, allocLoc, crcCheckCode, crcCheckCode.Length, out _))
+                throw new Exception($"Failed on WriteProcessMemory with System Error Code {Marshal.GetLastWin32Error()}");
+
+            Console.WriteLine($"CRC32 created at address {allocLoc.ToString("X")}");
+
+            
+            IntPtr hKernelbase = Libloaderapi.GetModuleHandleA("KERNELBASE.dll");
+            IntPtr isDebuggerPresentAddr = Libloaderapi.GetProcAddress(hKernelbase, "IsDebuggerPresent");
+
+
+            Console.WriteLine($"IsDebuggerPresent Addresss = {isDebuggerPresentAddr.ToString("X")}\n");
+
+       
+            //param passed to RCX
+            CRC32Check(isDebuggerPresentAddr);
+
+
+            while (true)
+            {
+                for (int i = 5; i > 0; i--)
+                {
+                    Console.WriteLine($"Next CRC32 Check in: {i}");
+                    Thread.Sleep(1000);
+                }
+
+
+                CRC32Check(isDebuggerPresentAddr);
+                Thread.Sleep(1000);
+
+            }
+        }
+        public static void CRC32Check(IntPtr isDebuggerPresentAddr)
+        {
+            IntPtr thread = Processthreadsapi.CreateRemoteThread(Process.GetCurrentProcess().Handle, IntPtr.Zero, 0, allocLoc, isDebuggerPresentAddr, 0, out IntPtr threadId);
+            Thread.Sleep(100);
+
+            if (!Processthreadsapi.GetExitCodeThread(thread, out uint exitCode))
+                throw new Exception($"GetExitCodeThread failed with System Error Code {Marshal.GetLastWin32Error()}");
+
+            if (exitCode == 259)
+            {
+                Console.WriteLine("Waiting for CRC Thread to finish.");
+                for (int i = 1; i <= 10; i++)
+                {
+                    Processthreadsapi.GetExitCodeThread(thread, out exitCode);
+                    if (exitCode != 259)
+                        break;
+
+                    if (i == 10)
+                        throw new Exception($"10 seconds have passed and the thread is still running. Something is wrong. Last recorded error code: {Marshal.GetLastWin32Error()}");
+
+                    Thread.Sleep(1000);
+                }
+            }
+            Console.WriteLine($"CRC32 value of {isDebuggerPresentAddr.ToString("X")} + 9 is {exitCode}\n");
+            Thread.Sleep(1000);
+        }
+    }
+}
