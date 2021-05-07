57e0a9a9d7785a94ee327e169d7e244632ceb1dd
Evulpes <54101182+Evulpes@users.noreply.github.com>
2021-05-07 22:24:10 +0100

    Algorithm implementation for any given number of bytes.

--- CRC32.cs
+++ CRC32.cs
@@ -2,71 +2,193 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using System.Linq;
 namespace CRC32
 {
     class CRC32 : NativeMethods
     {
-        delegate int crcFunctionDelegate(IntPtr addr, int size);
+        delegate int crcFunctionDelegate(IntPtr addr, int[] registerLoops /*int size*/);
 
-        public static int AccumulateAtAddress(IntPtr address, uint crcSize=8)
+        public static int AccumulateAtAddress(IntPtr address, uint crcSize)
         {
-            if (crcSize < 8 || (crcSize % 8) != 0)
-                throw new NotImplementedException();
+            Dictionary<string, int> requiredRegisters = new()
+            {
+                { "RAX", 1 },
+                { "EAX", 0 },
+                { "AX", 0 },
+                { "AL", 0 },
+            };
+            #region crcprologue
+            List<byte> crcInstructions = new ()
+            {
+                0x48, 0x31, 0xc0    //xor rax, rax
+            };
+            #endregion
+            if (crcSize != 8)
+            {
+                requiredRegisters = CalculatorRegisterCount((int)crcSize);
+                crcInstructions.AddRange
+                (
+                    new byte[] 
+                    { 
+                        0x44, 0x8b, 0x0a,                   //mov r9d, [rdx]
+                        0x4c, 0x8b, 0x19,                   //mov r11 [rcx]
+                        0xf2, 0x49, 0x0f, 0x38, 0xf1, 0xc3, //crc rax, r11
+                        0x48, 0x83, 0xc1, 0x08,             //add rcx, 8
+                        0x49, 0xff, 0xc9,                   //dec r9
+                        0x49, 0x83, 0xf9, 0x00,             //cmp r9, 0
+                        0x75, 0xea                          //jne -0x14
+                    } 
+                );
+                if (requiredRegisters["EAX"] != 0)
+                {
+                    crcInstructions.AddRange
+                    (
+                        new byte[]
+                        {
+                            0x44, 0x8b, 0x4a, 0x04,             //mov r9d, [rdx+4]
+                            0x44, 0x8b, 0x19,                   //mov r11d [rcx]
+                            0xf2, 0x49, 0x0f, 0x38, 0xf1, 0xc3, //crc rax, r11
+                            0x48, 0x83, 0xc1, 0x04,             //add rcx, 4
+                            0x49, 0xff, 0xc9,                   //dec r9
+                            0x49, 0x83, 0xf9, 0x00,             //cmp r9, 0
+                            0x75, 0xea                          //jne -0x14
+                        }
+                    );
+                }
+                if (requiredRegisters["AX"] != 0)
+                {
+                    crcInstructions.AddRange
+                    (
+                        new byte[]
+                        {
+                            0x44, 0x8b, 0x4a, 0x04,             //mov r9d, [rdx+4]
+                            0x66, 0x44, 0x8b, 0x19,             //mov r11w, [rcx]
+                            0xf2, 0x49, 0x0f, 0x38, 0xf1, 0xc3, //crc rax, r11
+                            0x48, 0x83, 0xc1, 0x02,             //add rcx, 2
+                            0x49, 0xff, 0xc9,                   //dec r9
+                            0x49, 0x83, 0xf9, 0x00,             //cmp r9, 0
+                            0x75, 0xea                          //jne -0x14
+                        }
+                    );
+                }
+                if (requiredRegisters["AL"] != 0)
+                {
+                    crcInstructions.AddRange
+                    (
+                        new byte[]
+                        {
+                            0x44, 0x8b, 0x4a, 0x04,             //mov r9d, [rdx+4]
+                            0x44, 0x8a, 0x19,                   //mov r11b, [rcx]
+                            0xf2, 0x49, 0x0f, 0x38, 0xf1, 0xc3, //crc rax, r11
+                            0x48, 0xff, 0xc1,                   //inc rcx
+                            0x49, 0xff, 0xc9,                   //dec r9
+                            0x49, 0x83, 0xf9, 0x00,             //cmp r9, 0
+                            0x75, 0xea                          //jne -0x14
+                        }
+                    );
+                }
+
+            }
+
+
+
 
             int eightRemain = (int)crcSize % 8;
             int eightMultiple = (int)crcSize / 8;
 
-            List<byte> crcInstructions = new List<byte>()
-            {
-                0x48, 0x31, 0xc0    //xor rax, rax
-            };
-            if (crcSize > 8)
-            {
-                crcInstructions.AddRange
-                (
-                    new byte[] { 0x49, 0x89, 0xd1 } //mov r9, rdx
-                );
-            }
-
-
-            crcInstructions.AddRange
-            (
-                new byte[]
-                {
-                    0x4c, 0x8b, 0x19,                   //mov r11 [rcx]
-                    0xf2, 0x49, 0x0f, 0x38, 0xf1, 0xc3  //crc rax, r11
-                }
-            );
-
-            if (crcSize > 8)
-            {
-                crcInstructions.AddRange
-                (
-                    new byte[]
-                    {
-                        0x48, 0x83, 0xc1, 0x08, //add rcx, 8
-                        0x49, 0xff, 0xc9,       //dec r9
-                        0x49, 0x83, 0xf9, 0x00, //cmp r9, 0
-                        0x75, 0xea              //jne -0x14
-                        
-                    }
-                );
-            }
+            #region crcepilogue
             crcInstructions.Add(0xC3);  //ret
+            #endregion
+
             byte[] assembly = crcInstructions.ToArray();
 
             IntPtr allocLoc = Memoryapi.VirtualAlloc(IntPtr.Zero, (uint)assembly.Length, Winnt.AllocationType.MEM_COMMIT, Winnt.MemoryProtection.PAGE_EXECUTE_READWRITE);
             Memoryapi.WriteProcessMemory(Process.GetCurrentProcess().Handle, allocLoc, assembly, assembly.Length, out IntPtr _);
 
-#if DEBUG
-            Console.WriteLine($"Loc: {allocLoc.ToString("X")}");
-            Console.Read();
-#endif            
-            int result = ((crcFunctionDelegate)Marshal.GetDelegateForFunctionPointer(allocLoc, typeof(crcFunctionDelegate)))(address, eightMultiple);
 
-            Memoryapi.VirtualFree(allocLoc, 0, 0x00004000); //MEM_DECOMMIT - LAZY! fix.
+            Console.WriteLine($"LocDB: {allocLoc:X}");
+            Console.Read();
+     
+            int result = ((crcFunctionDelegate)Marshal.GetDelegateForFunctionPointer
+            (
+                allocLoc, 
+                typeof(crcFunctionDelegate))
+            )
+            (
+                address, 
+                new int[] 
+                { 
+                    requiredRegisters["RAX"], 
+                    requiredRegisters["EAX"], 
+                    requiredRegisters["AX"], 
+                    requiredRegisters["AL"] 
+                }
+            );
+
+            bool testme = Memoryapi.VirtualFree(allocLoc, 0, 0x00004000); //MEM_DECOMMIT - LAZY! fix.
 
             return result;
+        }
+        private static Dictionary<string, int> CalculatorRegisterCount(int x)
+        {
+            List<byte> combinations = new();
+            Dictionary<string, int> registerCount = new()
+            {
+                { "RAX", 0 },
+                { "EAX", 0 },
+                { "AX", 0},
+                { "AL", 0}
+            };
+            byte[] values = new byte[]
+            {
+                8, 4, 2, 1
+            };
+            return RecursiveRegisterAccumulator(x, out _);
+
+            Dictionary<string, int> RecursiveRegisterAccumulator(int x, out int i)
+            {
+                for (i = 0; i < values.Length; i++)
+                {
+                    int remain = x % values[i];
+                    int multiple = x / values[i];
+
+                    if (multiple == 0)
+                        continue;
+
+                    if (multiple >= 1)
+                    {
+                        switch (values[i])
+                        {
+                            case 8:
+                                registerCount["RAX"] += multiple;
+                                break;
+                            case 4:
+                                registerCount["EAX"] += multiple;
+                                break;
+                            case 2:
+                                registerCount["AX"] += multiple;
+                                break;
+                            case 1:
+                                registerCount["AL"] += multiple;
+                                break;
+                        }
+                    }
+
+                    if (remain == 1 && multiple == 0)
+                        registerCount["AL"]++;
+                    else if (remain >= 1)
+                        RecursiveRegisterAccumulator(remain, out i);
+                    
+                    if (remain == 0)
+                    {
+                        i = 4;
+                        return registerCount;
+                    }
+
+                }
+                return registerCount;
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,37 +2,28 @@
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Threading;
-
+using System.Collections.Generic;
 namespace CRC32
 {
     class Program : NativeMethods
     {
-        delegate int TestDelegate(IntPtr isDebuggerPresentAddr);
-        static IntPtr allocLoc;
-        static readonly byte[] crcCheckCode = new byte[]
-        {
-            0x48, 0x31, 0xc0,                               //xor rax, rax
-            0x48, 0x8b, 0x49, 0x09,                         //mov rcx, [rcx+8]
-            0xf2, 0x48, 0x0f, 0x38, 0xf1, 0xc1,             //crc32 rax, rcx
-            0xc3                                            //ret
-        };
         static void Main()
-        {            
+        {
             IntPtr hKernelbase = Libloaderapi.GetModuleHandleA("KERNELBASE.dll");
             IntPtr isDebuggerPresentAddr = Libloaderapi.GetProcAddress(hKernelbase, "IsDebuggerPresent");
 
-            
+            int c = 1;
             while (true)
             {
-                int c = 1;
-                int crcVal = CRC32.AccumulateAtAddress(isDebuggerPresentAddr, 16);
-              
-                Console.WriteLine($"{c}: {crcVal} (IDP: {isDebuggerPresentAddr.ToString("X")})");
-                Console.ReadLine();
 
+                int crcVal = CRC32.AccumulateAtAddress(isDebuggerPresentAddr, 15);
+
+                Console.WriteLine($"{c}: {crcVal} (IDP: {isDebuggerPresentAddr:X})");
+                Console.ReadLine();
+                c++;
             };
 
             //Free ModuleHandle.
-        
+        }
     }
 }
